27e404002b22027d80dfbe2dfdc2d0c69d55019c
Mike Cremer <pcfreak9000@gmail.com>
2017-07-19 19:53:01 +0200

    Added copyright notice for Active Directory Object Picker

--- DaemonMaster/CreditsWindow.xaml.cs
+++ DaemonMaster/CreditsWindow.xaml.cs
@@ -15,7 +15,9 @@ namespace DaemonMaster
                 "The program \"DeamonMaster\" was originally created by MCPC10 (Main Developer) and Stuffi3000 \n\n GUI: Stuffi3000, MCPC10 \n Code: MCPC10 \n Icon: Stuffi3000 \n\n" +
                 "Used Librarys: \n" +
                 "=> Newtonsoft.Json - James Newton - King - MIT License \n" +
-                "=> NLog - Jaroslaw Kowalski, Kim Christensen, Julian Verdurmen - BSD 3 - clause \"New\" or \"Revised\" License \n\n" +
+                "=> NLog - Jaroslaw Kowalski, Kim Christensen, Julian Verdurmen - BSD 3 clause \"New\" or \"Revised\" License \n" +
+                "=> AutoUpdater.NET - RBSoft - MIT License \n" +
+                "=> Active Directory Object Picker - Tulpep - MS-PL License \n\n" +
                 "Thanks to: \n Pinvoke.net \n stackoverflow.com (for help from the users) \n entwickler-ecke.de (for help from the users)";
         }
     }
