ee5b32221e70ded59da10535c2de94189f9c892b
Mike Cremer <8211507+MCPC10@users.noreply.github.com>
2020-09-20 11:14:45 +0200

    version bump

--- DaemonMasterService/Properties/AssemblyInfo.cs
+++ DaemonMasterService/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DaemonMasterService")]
-[assembly: AssemblyCopyright("Copyright © 2017-2019 Mike Cremer / Stephan André")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 Mike Cremer / Stephan André")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.10.0")]
-[assembly: AssemblyFileVersion("0.2.10.0")]
+[assembly: AssemblyVersion("0.2.11.0")]
+[assembly: AssemblyFileVersion("0.2.11.0")]
