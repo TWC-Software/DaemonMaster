2ec5c2a96dfe42fee10ac3918a0b84cd42022d9c
Mike Cremer <8211507+MCPC10@users.noreply.github.com>
2025-03-22 19:03:45 +0100

    Make old service item nullable

--- DaemonMaster/Utilities/Messages/UpdateServiceItemMessage.cs
+++ DaemonMaster/Utilities/Messages/UpdateServiceItemMessage.cs
@@ -5,10 +5,10 @@ namespace DaemonMaster.Utilities.Messages
     public class UpdateServiceItemMessage
     {
         public object Sender { get; }
-        public ServiceListViewItem OldServiceItem { get; }
+        public ServiceListViewItem? OldServiceItem { get; }
         public ServiceListViewItem NewServiceItem { get; }
         
-        public UpdateServiceItemMessage(object sender, ServiceListViewItem oldItem, ServiceListViewItem newItem)
+        public UpdateServiceItemMessage(object sender, ServiceListViewItem? oldItem, ServiceListViewItem newItem)
         {
             Sender = sender;
             OldServiceItem = oldItem;
